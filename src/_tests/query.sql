1f42257164445da068ed188e37c7e9e4255ee623
eliandrosergio <eliandrosergio42@gmail.com>
2025-09-27 15:40:01 +0100

    removendo os tests

--- src/_tests/query.sql
+++
@@ -1,7 +0,0 @@
--- SQLite
-
--- Consultar registros
-SELECT * FROM alunos;
-
--- Alter valor de registros
--- UPDATE alunos SET telefone_encarregado='932035303' WHERE telefone_encarregado='+244 932 035 303'; 
