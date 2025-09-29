743f4c104f33706a7052e7c104496d78359cc7cf
eliandrosergio <eliandrosergio42@gmail.com>
2025-09-29 03:38:19 +0100

    melhoramento do metodo de autenticação usando o token de aceso entre o cliente e o servidos, e no desing em si

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -13,14 +13,16 @@ const authMiddleware = (req, res, next) => {
                 req.cookies.authToken;
 
     if (!token) {
-        return res.status(401).json({ error: 'Acesso negado. Token não fornecido.' });
+        return res.render('home', { title: 'Gestão de Transporte Escolar' });
+        //return res.status(401).json({ error: 'Acesso negado. Token não fornecido.' });
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded; // Adiciona user ao request
         next();
     } catch (err) {
-        res.status(401).json({ error: 'Token inválido.' });
+        res.render('navViews/logout', { title: 'Logout' }); // Redireciona para login
+        //res.status(401).json({ error: 'Token inválido.' });
     }
 };
 
